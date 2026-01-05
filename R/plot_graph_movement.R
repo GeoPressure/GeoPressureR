af38b4f5e22d38cdb7399a97c0ce273dfd902605
Raphaël Nussbaumer <rafnuss@gmail.com>
2026-01-05 17:24:54 +0100

    Improve plot_graph_movement visualization and annotations
    Refactors the plotting logic to better distinguish between line and point data, adds conditional vertical lines and annotations for low_speed_fix and zero_speed_ratio, and updates colors and line widths for improved clarity. Removes unnecessary data frame and streamlines ggplot2 calls.

--- R/plot_graph_movement.R
+++ R/plot_graph_movement.R
@@ -56,11 +56,10 @@ plot_graph_movement <- function(
 
   d <- data.frame(
     speed = speed,
-    prob = speed2prob(speed, movement)
+    prob = speed2prob(speed, movement),
+    line = movement$low_speed_fix == 0 | speed > 0
   )
-  lsf <- data.frame(
-    low_speed_fix = movement$low_speed_fix
-  )
+  max_prob <- max(d$prob, na.rm = TRUE)
 
   type <- movement$type
   if (is.null(type) && !is.null(movement$method) && movement$method == "power") {
@@ -74,19 +73,52 @@ plot_graph_movement <- function(
 
   p <- ggplot2::ggplot() +
     ggplot2::geom_line(
-      data = d,
+      data = d[d$line, ],
       ggplot2::aes(x = .data$speed, y = .data$prob),
-      color = "grey"
-    ) +
-    ggplot2::geom_vline(
-      data = lsf,
-      ggplot2::aes(xintercept = .data$low_speed_fix),
-      color = "red"
+      color = "#2F2F2F",
+      linewidth = 1.5
     ) +
     ggplot2::theme_bw() +
     ggplot2::scale_y_continuous(name = "Probability") +
     ggplot2::scale_x_continuous(name = xlab) +
     ggplot2::theme(legend.position = "none")
+
+  if (movement$low_speed_fix > 0) {
+    p <- p +
+      ggplot2::geom_vline(
+        ggplot2::aes(xintercept = movement$low_speed_fix),
+        color = "#1B9E77",
+        linewidth = 0.7
+      ) +
+      ggplot2::annotate(
+        "text",
+        x = movement$low_speed_fix,
+        y = max_prob * 0.5,
+        label = paste0("low_speed_fix: ", movement$low_speed_fix),
+        color = "#1B9E77",
+        size = 3,
+        hjust = -0.1
+      )
+  }
+
+  if (movement$zero_speed_ratio > 0) {
+    p <- p +
+      ggplot2::geom_point(
+        data = d[!d$line, ],
+        ggplot2::aes(x = .data$speed, y = .data$prob),
+        color = "#D95F02",
+        size = 3.2
+      ) +
+      ggplot2::annotate(
+        "text",
+        x = 0,
+        y = max(d$prob[!d$line]),
+        label = paste0("zero_speed_ratio: ", movement$zero_speed_ratio),
+        color = "#D95F02",
+        size = 3,
+        hjust = -0.1
+      )
+  }
 
   if (plot_plotly) {
     return(plotly::ggplotly(p, dynamicTicks = TRUE))
