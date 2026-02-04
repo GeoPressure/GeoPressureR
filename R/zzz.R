23e1829911d35c31f935f8262b52c11e20dc3ab4
Raphaël Nussbaumer <rafnuss@gmail.com>
2026-02-04 21:48:53 +0100

    move internal data loader in zzz

--- R/zzz.R
+++ R/zzz.R
@@ -28,3 +28,15 @@ format_minutes <- function(mins) {
     "{minutes}min"
   )
 }
+
+#' @noRd
+.onLoad <- function(libname, pkgname) {
+  data_dir <- system.file("extdata/internal", package = pkgname)
+  pkg_env <- asNamespace(pkgname)
+  assign(
+    "pressurepath_variable",
+    readRDS(file.path(data_dir, "pressurepath_variable.rds")),
+    envir = pkg_env
+  )
+  assign("avonet", readRDS(file.path(data_dir, "avonet.rds")), envir = pkg_env)
+}
